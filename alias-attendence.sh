6ea8c8815b5c0ba3355e0758a82026bb0670808b
rajsamnani667 <78310817+rajsamnani667@users.noreply.github.com>
2021-08-21 10:16:37 +1100

    Update alias-attendence.sh

--- alias-attendence.sh
+++ alias-attendence.sh
@@ -2,56 +2,103 @@
 
 
 alias attendence='
-
-                  read -p "Enter 1st date: " Date1 
-                  
-                  read -p "Enter 2nd date: " Date2 
-                  
-                  
-                  while read a1 a2 a3 a4
+              start_date=$1
+              end_date=$2
+              i=0
+              t1=0
+              if [ -z $startdate ] && [ -z $enddate ]
+              then
+                  while input=' ' read -r col1 col2 col3 col4
                   do
-                  
-                  
-                  for u in {01..30..1}
+                      if [ $i -le 0 ]
+                      then
+                          date="${col3%,}"
+                          i=99
+                      fi
+                      if [ "${col3%,}" = "$date" ]
+                      then
+                          currentdate=$col3
+                          echo $col1 >> temp.txt
+                      else
+                          date="${col3%,}"
+                          for user in `cat users.txt`
+                          do
+                              for users in `cat temp.txt`
+                              do
+                                  p=0 #present
+                                  if [ "$users" = "$user" ]
+                                  then
+                                      p=1
+                                      break
+                                  fi
+                              done
+                              if [ $p -le 0 ]
+                              then
+                                  echo "$user was absent on ${currentdate%,}"
+                              fi
+                          done
+                          >temp.txt
+                      fi
+                  done < attendance.log
+                  for user in `cat users.txt`
                   do
-                    if [ "$a3" = "$Date1," -a "$a1" = "sysAd_$u" -o "$a3" = "$Date2," -a "$a1" = "sysAd_$u" ]; 
-                    then
-                       echo "$a1"
-                    else
-                       if [ "$a1" != "sysAd_$u" ];
-                       then    
-                           echo "sysAd_$u" "$a2" "$a3"
-                       fi
-                    fi
-                  done   
-                  
-                       
-                  for u in {01..30..1}
+                      for users in `cat temp.txt`
+                      do
+                          p=0
+                          if [ "$users" = "$user" ]
+                          then
+                              p=1
+                              break
+                          fi
+                      done
+                      if [ $p -le 0 ]
+                      then
+                          echo "$user was absent on ${currentdate%,}"
+                      fi
+                  done
+                  >temp.txt
+                  rm -rf ./temp.txt
+              else
+                  while input=' ' read -r col1 col2 col3 col4
                   do
-                    if [ "$a3" = "$Date1," -a "$a1" = "webdev_$u" -o "$a3" = "$Date2," -a "$a1" = "webdev_$u" ]; 
-                    then
-                       echo "$a1"
-                    else
-                       if [ "$a1" != "webdev_$u" ];
-                       then    
-                           echo "webdev_$u" "$a2" "$a3"
-                       fi
-                    fi
-                  done   
-                  
-                  
-                  for u in {01..30..1}
-                  do
-                    if [ "$a3" = "$Date1," -a "$a1" = "appdev_$u" -o "$a3" = "$Date2," -a "$a1" = "appdev_$u" ]; 
-                    then
-                       echo "$a1"
-                    else
-                       if [ "$a1" != "appdev_$u" ];
-                       then    
-                           echo "appdev_$u" "$a2" "$a3"
-                       fi
-                    fi
-                  done   
-                  
-                  done < attendence.log
-                  '
+                      if [ $i -le 0 ]
+                      then
+                        date="$1"
+                        i=99
+                      fi
+                      if [ "${col3%,}" = "$date" ]
+                      then
+                          currentdate=$col3
+                          echo $col1 >> temp.txt
+                      else
+                          date="${col3%,}"
+                          if [ "${col3%,}" = "$2" ]
+                          then
+                              t1=1
+                          fi
+                          for user in `cat users.txt`
+                          do
+                              for users in `cat temp.txt`
+                              do
+                                  p=0 #present
+                                  if [ "$users" = "$user" ]
+                                  then
+                                      p=1
+                                      break
+                                  fi
+                              done
+                              if [ $p -le 0 ]
+                              then
+                                  echo "$user was absent on ${currentdate%,}"
+                              fi
+                          done
+                          >temp.txt
+                          if [ $t1 -ge 1 ] && [ "${col3%,}" != "$2" ]
+                          then
+                              rm -rf ./temp.txt
+                              exit
+                          fi
+                      fi
+                  done < attendance.log   
+              fi
+              '
